e52aa27cb51ca2e011502c8b60b32c40bf21b38f
Armando Ibarra <aibarrakerzon@gmail.com>
2022-07-28 09:39:56 -0500

    Initial commit

---
+++ lib/checkout_sdk.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "checkout_sdk/version"
+
+module CheckoutSdk
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/checkout_sdk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CheckoutSdk do
+  it "has a version number" do
+    expect(CheckoutSdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
